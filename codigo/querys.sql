9437029226300b7c0520e608bc2a165f38d1eb3b
Ferreirabento <jf6808932@gmai.com>
2026-05-08 16:10:18 -0300

    estou usando esse banco de dados para a analise e logo vou fazer a limpeza dos dados brutos para jogar no Power BI

---
+++ codigo/querys.sql
@@ -0,0 +1,7 @@
+--Banco de dados que será utilizado durante o projeto--
+USE desempenhosuperlojas;
+
+SELECT * FROM sales_performance_analysis;
+
+-- LIMPEZA DOS DADOS -- 
+SELECT  FROM sales_performance_analysis
