8e04963a9c48febab492a43dee4498dc99ae6ec4
Ferreirabento <jf6808932@gmai.com>
2026-05-11 17:05:41 -0300

    criei um arquivo de requisitos para esse projeto em word para saber o que tem que ser feito em relação as analises.
    sobre a analise exploratoria, estou trabalhando na distribuião de frequencia. até esse momento estou usando uma formula matematica (regra de sturges) para definir o numero de classes.

--- codigo/querys.sql
+++ codigo/querys.sql
@@ -4,8 +4,12 @@ USE desempenhosuperlojas;
 SELECT * FROM sales_performance_analysis;
 
 -- Vou começar com uma análise exploratória usando estátistica --
--- vou começar com uma amostra
-
-SELECT *
-FROM sales_performance_analysis
+-- vou começar com uma amostra-- 
+SELECT * FROM sales_performance_analysis
 LIMIT 100;
+
+-- 895 LINHAS -- 
+SELECT COUNT(Store_Id)
+FROM sales_performance_analysis;
+
+-- K = 1 + 3,3 * LOG(N) = K = 1 + 3,3 * LOG 895 = 11
