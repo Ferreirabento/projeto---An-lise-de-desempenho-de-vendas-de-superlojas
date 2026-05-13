bb93d21a8131b45b046dfb68670a4998d1729c54
Ferreirabento <jf6808932@gmai.com>
2026-05-13 16:43:10 -0300

    organizando as analises - verificação de nulos - calculo de classes     - numero     - amplitude total     - amplitude de classes
    to fazendo em uma amostra: no caso, to pegando só uma coluna que é a Store_sales.

--- codigo/querys.sql
+++ codigo/querys.sql
@@ -1,16 +1,34 @@
 --Banco de dados que será utilizado durante o projeto--
 USE desempenhosuperlojas;
 
-SELECT * FROM sales_performance_analysis;
-
 -- Vou começar com uma análise exploratória usando estátistica --
 -- vou começar com uma amostra-- 
-SELECT * FROM sales_performance_analysis
-LIMIT 100;
+SELECT * FROM sales_performance_analysis;
 
--- 895 LINHAS x 6 COLUNAS = 5.370 -- 
-SELECT COUNT(*) 
+--verificando se não tem null--
+SELECT * 
+FROM sales_performance_analysis
+WHERE Store_Sales IS NULL;
+
+-- calculando o numero de classe de store_sales -- 
+SELECT COUNT(Store_Sales)
 FROM sales_performance_analysis;
--- K = 1 + 3,3 * LOG(N) = K = 1 + 3,3 * LOG(5370) = 13 É O NUMERO DE CLASSES
+-- K = 1 + 3,3 * LOG(N) = K = 1 + 3,3 * LOG(895) = 11 É O NUMERO DE CLASSES
+
+-- calculando a amplitude total de store_sales = 101400 --
+SELECT MAX(Store_Sales) - MIN(Store_Sales) as amplitude_total
+FROM sales_performance_analysis;
+
+-- calculando a amplitude de classe de store_sales --
+-- ULTIMO OU LIMITE INFERIOR = 116320 --
+SELECT Store_Sales
+FROM sales_performance_analysis
+ORDER BY Store_Sales DESC;
+-- PRIMIERO OU LIMITE SUPERIOR = 14920 --
+SELECT Store_Sales
+FROM sales_performance_analysis
+ORDER BY Store_Sales ASC;
+
+
 
 
