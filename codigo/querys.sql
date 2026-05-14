d7a959faabad51f79f6b5503c7f3b952d345cf09
Ferreirabento <jf6808932@gmai.com>
2026-05-14 16:13:19 -0300

    com isso, fiz o calculo de amplitude de classes de store_sales

--- codigo/querys.sql
+++ codigo/querys.sql
@@ -3,7 +3,8 @@ USE desempenhosuperlojas;
 
 -- Vou começar com uma análise exploratória usando estátistica --
 -- vou começar com uma amostra-- 
-SELECT * FROM sales_performance_analysis;
+SELECT *
+FROM sales_performance_analysis;
 
 -- verificando se não tem duplicatas --
 --verificando se não tem null--
@@ -16,21 +17,30 @@ WITH sales_performance_analysis AS (
 SELECT tratamento,
     CEIL(1 + (3.3 * LOG10(tratamento))) AS num_de_classes
 FROM sales_performance_analysis;
--- K = 1 + 3,3 * LOG(815) = 10 É O NUMERO DE CLASSES DE STORE_SALES --
+-- K = 1 + 3,3 * LOG(815) = 11 É O NUMERO DE CLASSES DE STORE_SALES --
 
 -- calculando a amplitude total de store_sales = 101400 --
 SELECT MAX(Store_Sales) - MIN(Store_Sales) as amplitude_total
 FROM sales_performance_analysis;
 
--- calculando a amplitude de classe de store_sales --
+-- calculando a amplitude de classe de store_sales  --
 -- ULTIMO OU LIMITE INFERIOR = 116320 --
 SELECT Store_Sales
 FROM sales_performance_analysis
 ORDER BY Store_Sales DESC;
-
 -- PRIMIERO OU LIMITE SUPERIOR = 14920 --
 SELECT Store_Sales
 FROM sales_performance_analysis
 ORDER BY Store_Sales ASC;
 
--- amplitude de classes de Store_sales --
+-- calculando a amplitude de classes de Store_sales --
+WITH sales_performance_analysis AS (
+    SELECT COUNT(Store_Sales) as num,
+        MAX(Store_Sales) - MIN(Store_Sales) as amplitude_total
+    FROM sales_performance_analysis
+    WHERE Store_Sales IS NOT null
+)
+SELECT amplitude_total,
+    CEIL(1 + (3.3 * LOG10(num))) as num_de_classes,
+    (amplitude_total / CEIL(1 + (3.3 * LOG10(num)))) as amplitude_de_classes
+FROM sales_performance_analysis;
